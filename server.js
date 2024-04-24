0bf16fee2c0d20e42837d42a6485560dd823b731
Evan Williams <elw4408@arizona.edu>
2024-04-24 00:33:03 -0700

    new security changes - EW

--- server.js
+++ server.js
@@ -22,6 +22,61 @@ const pool = new Pool(db);
 
 const app = express();
 
+// Security
+const crypto = require('crpyto')
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+const helmet = require('helmet');
+function firewall_detect(str_to_detect){
+    var regexp_rule =[
+        /select.+(from|limit)/i,
+        /(?:(union(.*?)select))/i,
+        /sleep\((\s*)(\d*)(\s*)\)/i,
+        /group\s+by.+\(/i,
+        /(?:from\W+information_schema\W)/i,
+        /(?:(?:current_)user|database|schema|connection_id)\s*\(/i,
+        /\s*or\s+.*=.*/i,
+        /order\s+by\s+.*--$/i,
+        /benchmark\((.*)\,(.*)\)/i,
+        /base64_decode\(/i,
+        /(?:(?:current_)user|database|version|schema|connection_id)\s*\(/i,
+        /(?:etc\/\W*passwd)/i,
+        /into(\s+)+(?:dump|out)file\s*/i,
+        /xwork.MethodAccessor/i,
+        /(?:define|eval|file_get_contents|include|require|require_once|shell_exec|phpinfo|system|passthru|preg_\w+|execute|echo|print|print_r|var_dump|(fp)open|alert|showmodaldialog)\(/i,
+        /\<(iframe|script|body|img|layer|div|meta|style|base|object|input)/i,
+        /(onmouseover|onmousemove|onerror|onload)\=/i,
+        /javascript:/i,
+        /\.\.\/\.\.\//i,
+        /\|\|.*(?:ls|pwd|whoami|ll|ifconfog|ipconfig|&&|chmod|cd|mkdir|rmdir|cp|mv)/i,
+        /(?:ls|pwd|whoami|ll|ifconfog|ipconfig|&&|chmod|cd|mkdir|rmdir|cp|mv).*\|\|/i,
+        /(gopher|doc|php|glob|file|phar|zlib|ftp|ldap|dict|ogg|data)\:\//i
+    ];
+    for(i=0; i< regexp_rule.length; i++){
+        if(regexp_rule[i].test(str_to_detect) == true){
+            console.log("Intercepted Attack:", "("+i+")", regexp_rule[i]);
+            return true;
+        }
+    }
+    return false;
+}
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+function decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
 
 //Middlware
 app.use(express.json()); 
@@ -29,7 +84,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'html');
 //app.engine('html', require('ejs').renderFile);
 app.use(cookieParser());
-
+app.use(helmet());
 
 //Sessions
 app.use(session({
@@ -62,19 +117,37 @@ app.post('/login', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     let useremail = req.body.useremail;
     let userpassword = req.body.password;
+
+    // Firewall Implementation
+    if(firewall_detect(useremail) == false && firewall_detect(userpassword) == false){
+        next();
+    }else{
+        res.send("Attack detected, intercepted")
+    }
+
+
     pool.connect(function (error, client, done){
         if(error){
             console.log(error);
         }else{
             let loginquery = "SELECT * FROM users WHERE email = '"+useremail+"'";
+
+            
+
             client.query(loginquery, function(error, results){
+
                 done();
+
                 if(error){
                     throw error;
                 };
+
                 let resultUser = results.rows;
                 console.log(resultUser[0]);
-                if(resultUser && resultUser[0]['password'] == userpassword){
+
+                let password = decrypt(resultUser[0]['password'])
+            
+                if(resultUser && password == userpassword){
                     req.session.user = resultUser;
                     app.locals.username = resultUser[0]['email'];
                     app.locals.login = true;
@@ -84,13 +157,13 @@ app.post('/login', (req, res) => {
                     res.send(JSON.stringify(dataSend));
                     console.log("Login Sent!");
                 }
-                if(resultUser && resultUser[0]['password'] != userpassword){
+                if(resultUser && password != userpassword){
                     var dataSend = {"login": "The password you have entered is incorrect!"};
                     console.log(JSON.stringify(dataSend));
                     res.send(JSON.stringify(dataSend));
                     console.log("Login Sent!");
                 }
-                if(resultUser.length == 0 && resultUser[0]['password'] != userpassword){
+                if(resultUser.length == 0 && password != userpassword){
                     var dataSend = {"login": "The username and/or password is incorrect!"};
                     console.log(JSON.stringify(dataSend));
                     res.send(JSON.stringify(dataSend));
@@ -119,6 +192,15 @@ app.post('/createaccount', (req, res) => {
     let useremail = req.body.useremail;
     let userpassword = req.body.password;
     let major = req.body.major;
+
+    // Firewall Implementation
+    if(firewall_detect(useremail) == false && firewall_detect(userpassword) == false && firewall_detect(major) == false){
+        next();
+    }else{
+        res.send("Attack detected, intercepted")
+    }
+
+
     pool.connect(function (error, client, done){
         if(error){
             console.log(error);
@@ -138,7 +220,12 @@ app.post('/createaccount', (req, res) => {
                     console.log("Account creation not successful!");
                 }else{
                     let createAccountQuery = "INSERT INTO users (email, password, major, notify) VALUES ($1, $2, $3, $4)";
-                    client.query(createAccountQuery, [useremail, userpassword, major, true], (error, results)=>{
+
+                    let encryptedEmail = encrypt(useremail)
+                    let encryptedPassword = encrypt(userpassword);
+                    let encryptedMajor = encrypt(major);
+                    
+                    client.query(createAccountQuery, [encryptedEmail, encryptedPassword, encryptedMajor, true], (error, results)=>{
                         done();
                         if(error){
                             throw error;
@@ -164,6 +251,15 @@ app.post('/forgotpassword', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     let useremail = req.body.useremail;
     let userpassword = req.body.newpassword;
+
+    // Firewall Implementation
+    if(firewall_detect(useremail) == false && firewall_detect(userpassword) == false){
+        next();
+    }else{
+        res.send("Attack detected, intercepted")
+    }
+
+
     pool.connect(function (error, client, done){
         if(error){
             console.log(error);
@@ -183,7 +279,11 @@ app.post('/forgotpassword', (req, res) => {
                     console.log("Password rest not successful!");
                 }else{
                     let updatePasswordQuery = "UPDATE users SET password = $1 WHERE email = $2";
-                    client.query(updatePasswordQuery, [userpassword, useremail], (error, results)=>{
+
+                    let encryptedPassword = encrypt(userpassword);
+                    let encryptedEmail = encrypt(useremail);
+                    
+                    client.query(updatePasswordQuery, [encryptedPassword, encryptedEmail], (error, results)=>{
                         done();
                         if(error){
                             throw error;
@@ -223,6 +323,15 @@ app.post("/dataformresultsforalgorithm", (req, res) => {
     let dataString = req.body.data;
     let artsString = req.body.arts;
     let societyString = req.body.society;
+
+    // Firewall Implementation
+    if(firewall_detect(type) == false && firewall_detect(dataString) == false && firewall_detect(artsString) == false && firewall_detect(societyString) == false){
+        next();
+    }else{
+        res.send("Attack detected, intercepted")
+    }
+
+
     console.log(dataString);
     const spawner = require('child_process').spawn;
 
@@ -246,7 +355,10 @@ app.post("/dataformresultsforalgorithm", (req, res) => {
                     console.log(resultUser);
                     if(resultUser.length > 0){
                         let updatecourseQuery = "UPDATE courserecs SET courserecstring = $1 WHERE userid = $2";
-                        client.query(updatecourseQuery, [course_matches, req.session.user[0]['userid']], (error, results)=>{
+
+                        let encryptedMatches = encrypt(course_matches);
+                        
+                        client.query(updatecourseQuery, [encryptedMatches, req.session.user[0]['userid']], (error, results)=>{
                             if(error){
                                 throw error;
                             };
@@ -261,7 +373,10 @@ app.post("/dataformresultsforalgorithm", (req, res) => {
                         });
                     }else{
                         let createCourseQuery = "INSERT INTO courserecs (userid, courserecstring) VALUES ($1, $2)";
-                        client.query(createCourseQuery, [req.session.user[0]['userid'], course_matches], (error, results)=>{
+
+                        let encryptedMatches = encrypt(course_matches);
+
+                        client.query(createCourseQuery, [req.session.user[0]['userid'], encryptedMatches], (error, results)=>{
                             done();
                             if(error){
                                 throw error;
@@ -330,6 +445,14 @@ app.post("/saveinterstformsummary", (req, res)=>{
     res.setHeader('Content-Type', 'text/html');
     let interstform = req.body.interestform;
 
+    // Firewall Implementation
+    if(firewall_detect(interstform) == false){
+        next();
+    }else{
+        res.send("Attack detected, intercepted")
+    }
+
+
     pool.connect(function (error, client, done){
         if(error){
             console.log(error);
@@ -343,7 +466,10 @@ app.post("/saveinterstformsummary", (req, res)=>{
                 console.log(resultUser);
                 if(resultUser.length > 0){
                     let updateInterestsQuery = "UPDATE interestresults SET interstsumarystring = $1 WHERE userid = $2";
-                    client.query(updateInterestsQuery, [interstform, req.session.user[0]['userid']], (error, results)=>{
+
+                    let encryptedIntForm = encrypt(interstform);
+                    
+                    client.query(updateInterestsQuery, [encryptedIntForm, req.session.user[0]['userid']], (error, results)=>{
                         if(error){
                             throw error;
                         };
@@ -358,7 +484,10 @@ app.post("/saveinterstformsummary", (req, res)=>{
                     });
                 }else{
                     let createInterstQuery = "INSERT INTO interestresults (userid, interstsumarystring) VALUES ($1, $2)";
-                    client.query(createInterstQuery, [req.session.user[0]['userid'], interstform], (error, results)=>{
+
+                    let encryptedIntForm = encrypt(interstform);
+                    
+                    client.query(createInterstQuery, [req.session.user[0]['userid'], encryptedIntForm], (error, results)=>{
                         done();
                         if(error){
                             throw error;
@@ -426,6 +555,15 @@ app.post("/savefouryearplan", (req, res)=>{
     res.setHeader('Content-Type', 'text/html');
     let savedPlan = req.body.fouryearplan;
 
+    // Firewall Implementation
+    if(firewall_detect(savedPlan) == false){
+        next();
+    }else{
+        res.send("Attack detected, intercepted")
+    }
+
+
+
     pool.connect(function (error, client, done){
         if(error){
             console.log(error);
@@ -439,7 +577,10 @@ app.post("/savefouryearplan", (req, res)=>{
                 console.log(resultUser);
                 if(resultUser.length > 0){
                     let updateInterestsQuery = "UPDATE fouryearplan SET planstring = $1 WHERE userid = $2";
-                    client.query(updateInterestsQuery, [savedPlan, req.session.user[0]['userid']], (error, results)=>{
+
+                    let encryptedPlan = encrypt(savedPlan);
+
+                    client.query(updateInterestsQuery, [encryptedPlan, req.session.user[0]['userid']], (error, results)=>{
                         if(error){
                             throw error;
                         };
@@ -454,7 +595,10 @@ app.post("/savefouryearplan", (req, res)=>{
                     });
                 }else{
                     let createInterstQuery = "INSERT INTO fouryearplan (userid, planstring) VALUES ($1, $2)";
-                    client.query(createInterstQuery, [req.session.user[0]['userid'], savedPlan], (error, results)=>{
+
+                    let encryptedSavedPlan = encrypt(savedPlan);
+                    
+                    client.query(createInterstQuery, [req.session.user[0]['userid'], encryptedSavedPlan], (error, results)=>{
                         done();
                         if(error){
                             throw error;
@@ -548,12 +692,26 @@ app.post('/updatesettings', (req, res) => {
     let updatePassword = req.body.password;
     let updateMajor = req.body.major;
     let updateNotify = req.body.notify;
+
+    // Firewall Implementation
+    if(firewall_detect(updateEmail) == false && firewall_detect(updatePassword) == false && firewall_detect(updateMajor) == false && firewall_detect(updateNotify) == false){
+        next();
+    }else{
+        res.send("Attack detected, intercepted")
+    }
+
+
     pool.connect(function (error, client, done){
         if(error){
             console.log(error);
         }else{
             let updateaccountQuery = "UPDATE users SET email = $1, password = $2, major = $3, notify = $4 WHERE userid = $5";
-            client.query(updateaccountQuery, [updateEmail, updatePassword, updateMajor, updateNotify, req.session.user[0]['userid']], (error, results) =>{
+
+            let encryptedEmail = encrypt(updateEmail);
+            let encryptedPassword = encrypt(updatePassword);
+            let encryptedMajor = encrypt(updateMajor);
+
+            client.query(updateaccountQuery, [encryptedEmail, encryptedPassword, encryptedMajor, updateNotify, req.session.user[0]['userid']], (error, results) =>{
                 done();
                 if(error){
                     throw error;
